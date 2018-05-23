c4744854f5530b1f2ec077ea8c0366888e126759
Sonaglio <Sonaglio@PC>
2018-05-22 21:02:15 -0300

    Inicial

---
+++ aula1/src/Exercicios/Exercicio1_1.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+
+public class Exercicio1_1 {
+        public static void main(String[] args) {
+        int valor1 = -2;
+        if (valor1 != 0){
+            
+            if (valor1 > 0){
+                System.out.println(valor1+" é positivo");
+            } else {
+                System.out.println(valor1+" é negativo");
+            }
+            
+                } else {
+            System.out.println("número é 0 ou nulo");
+        }
+        
+        
+    }
+}
